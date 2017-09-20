59fdd8fd61645c25c28ce828bfc2667274e0d853
Alex Pavluck <apavluck@gmail.com>
2017-09-20 15:44:45 -0400

    first commit

---
+++ app/models/article.rb
@@ -0,0 +1,11 @@
+class Article < ApplicationRecord
+
+  has_many :comments
+
+  validates :title, presence: true,
+                    length: { minimum: 5 }
+
+  validates :text,  presence: true,
+                    length: { minimum: 1 }
+
+end
